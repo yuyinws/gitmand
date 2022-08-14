c37add2fc153b0131f1c295fbe937841e5bf20d2
yuyinws <lyc657508366@gmail.com>
2022-08-14 17:58:08 +0800

    ci :test

--- packages/action/src/index.ts
+++ packages/action/src/index.ts
@@ -1,4 +1,4 @@
-import { readFile, writeFile } from 'fs/promises'
+import { readFile } from 'fs/promises'
 import * as core from '@actions/core'
 import type { List } from './render'
 import { render } from './render'
@@ -12,7 +12,6 @@ async function main() {
     const content = await readFile('gitmand.json', 'utf8')
     let lists: List[] = JSON.parse(content)
     lists = lists.slice(0, limit)
-    writeFile('test.svg', render(lists), 'utf8')
     core.setOutput('svg', render(lists))
   } catch (error) {
     core.error(`${error}`)
