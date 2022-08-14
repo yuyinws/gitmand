90f3d7fbe8bfd3b46eca1bbdc98e441a5e5a9b8a
yuyinws <lyc657508366@gmail.com>
2022-08-14 12:10:03 +0800

    ci: test

--- packages/action/src/index.ts
+++ packages/action/src/index.ts
@@ -1,1 +1,12 @@
-console.log('action')
+import { readFile } from 'fs/promises'
+
+async function main() {
+  try {
+    const content = await readFile('gitmand.json', 'utf8')
+    console.log('🚀 ~ file: index.ts ~ line 6 ~ main ~ content', content)
+  } catch (error) {
+    console.log('🚀 ~ file: index.ts ~ line 8 ~ main ~ error', error)
+  }
+}
+
+main()
